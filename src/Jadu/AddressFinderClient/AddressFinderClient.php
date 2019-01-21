7eb9dd3794210da06257dbce45701ac060709a3f
Jordan Moran <jordan.moran@mckennaconsultants.com>
2019-01-21 15:58:06 +0000

    added base testing class

---
+++ src/Jadu/AddressFinderClient/AddressFinderClient.php
@@ -0,0 +1,21 @@
+<?php
+   namespace Jadu\AddressFinderClient; 
+
+  class AddressFinderClient {
+    
+        private $configuration;
+        
+        function __construct() {
+            $strJsonFileContents = file_get_contents("Configuration/Configuration.json");
+            $this->configuration = $strJsonFileContents;
+        }
+
+        public function getAllProperties() {
+            return "TODO : Get All Properties";
+        }
+      
+        public function getSpecificProperty($identifier) {
+            return "TODO: Get Spefific Property ";
+        }
+    }
+?>

6f0bdddf687d24a2566b9ad802d753f04bae8574
Dean Sykes <dean.sykes@mckennaconsultants.com>
2019-01-24 10:16:47 +0000

    In progress

--- src/Jadu/AddressFinderClient/AddressFinderClient.php
+++ src/Jadu/AddressFinderClient/AddressFinderClient.php
@@ -3,6 +3,8 @@
 namespace Jadu\AddressFinderClient;
 
 use Jadu\AddressFinderClient\Model\AddressFinderClientConfigurationModel;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\MessageFactoryDiscovery;
 
 /**
  * AddressFinderClient.
@@ -17,18 +19,43 @@ class AddressFinderClient
     protected $configuration;
 
     /**
+     * @var HttpClient
+     */
+    protected $httpClient;
+
+    /**
+     * @var MessageFactory
+     */
+    protected $messageFactory;
+
+    /**
      * @param AddressFinderClientConfigurationModel $configuration
      */
     public function __construct(AddressFinderClientConfigurationModel $configuration)
     {
         $this->configuration = $configuration;
+        $this->httpClient = HttpClientDiscovery::find();
+        $this->messageFactory = MessageFactoryDiscovery::find();
     }
 
     /**
-     * Place holder Method.
+     * @param string $postCode
+     * 
+     * @return array|Address
      */
-    public function getAllProperties()
+    public function findPropertiesByPostCode(string $postCode)
     {
-        return 'TODO : Get All Properties';
+        $headers = array('X-Authentication-Key' => $this->configuration->apiKey);
+        $response = $this->httpClient->sendRequest(
+            $messageFactory->createRequest('GET', urlencode($this->configuration->baseUri . str_replace('{postCode}', $postcode, $this->configuration->propertyLookupSearchPath), $headers))
+        );
+
+        if ($response->getStatusCode() == 200)
+        {
+            // deserialise and return
+            return '200';
+        }
+
+        return 'Error';
     }
 }
--- tests/unit/AddressFinderClient/AddressFinderClientTest.php
+++ tests/unit/AddressFinderClient/AddressFinderClientTest.php
@@ -2,9 +2,50 @@
 
 namespace Jadu\Tests\AddressFinderClient;
 
+use Jadu\AddressFinderClient;
+use Jadu\AddressFinderClient\Model\AddressFinderClientConfigurationModel;
+use Http\Mock\Client;
+use PHPUnit_Framework_MockObject_MockObject;
+use PHPUnit_Framework_TestCase;
+
 class AddressFinderClientTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * PostCode.
+     */
+    const TEST_POSTCODE = 'LE19 1RJ';
+
+    /**
+     * @var AddressFinderClientConfigurationModel
+     */
+    private $configuration;
+
+    /**
+     * @var AddressFinderClient
+     */
+    private $addressFinderClient;
+
     protected function setUp()
     {
+        parent::setUp();
+
+        $this->configuration = new AddressFinderClientConfigurationModel();
+
+        $this->configuration->baseUri = 'http://localhost:8000';
+        $this->configuration->apiKey = 'testkey';
+        $this->configuration->statusPath = '/status';
+        $this->configuration->propertyLookupSearchPath = '/property/search/{postcode}';
+        $this->configuration->propertyLookupFetchPath = '/property/fetch/{identifier}';
+        $this->configuration->streetLookupSearchPath = '/street/search/{term}';
+        $this->configuration->streetLookupFetchPath = '/street/fetch/{identifier}';
+
+        $this->addressFinderClient = new AddressFinderClient(configuration);
+    }
+
+    public function testRequests()
+    {
+        $response = $this->createMock('Psr\Http\Message\ResponseInterface');
+
+        $results = $this->addressFinderClient->findPropertiesByPostCode(TEST_POSTCODE);
     }
 }
